29a6a2bf9efa24dd4c5c0c86bdf652e81239be2e
liqi <liqi@douyu.tv>
2019-02-14 13:18:00 +0800

    add comment

--- grpc/service.go
+++ grpc/service.go
@@ -7,23 +7,28 @@ import (
 	"google.golang.org/grpc"
 )
 
+// Service implements grpc service
 type Service struct {
 	server *grpc.Server
 }
 
+// NewService creates grpc service
 func NewService(opts ...grpc.ServerOption) *Service {
 	return &Service{server: grpc.NewServer(opts...)}
 }
 
+// Serve serves grpc service
 func (s *Service) Serve(net net.Listener) error {
 	return s.server.Serve(net)
 }
 
+// GracefulStop stops grpc service graceful
 func (s *Service) GracefulStop(ctx context.Context) error {
 	s.server.GracefulStop()
 	return nil
 }
 
+// Server returns grpc server
 func (s *Service) Server() *grpc.Server {
 	return s.server
 }
